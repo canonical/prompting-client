6dfa01ba3f848b70f20b56f9ac9992532a9e7083
ashuntu <ashton.nelson@canonical.com>
2025-04-22 13:58:16 -0500

    Update deprecated *Ref types

--- flutter/apps/prompting_client_ui/lib/app/prompt_model.dart
+++ flutter/apps/prompting_client_ui/lib/app/prompt_model.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:prompting_client/prompting_client.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:ubuntu_service/ubuntu_service.dart';
@@ -5,5 +6,5 @@ import 'package:ubuntu_service/ubuntu_service.dart';
 part 'prompt_model.g.dart';
 
 @riverpod
-PromptDetails currentPrompt(CurrentPromptRef ref) =>
+PromptDetails currentPrompt(Ref ref) =>
     getService<PromptDetails>();
